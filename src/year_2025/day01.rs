9db53f69086e4e15d54ae5e1ea28161801770eda
Virgile <virgile.theuerlacher@worldline.com>
2025-12-05 13:04:00 +0100

    2025 day 1

--- src/year_2025/day01.rs
+++ src/year_2025/day01.rs
@@ -1,20 +1,72 @@
 use crate::day::Day;
 
-pub struct Day01 {}
+#[derive(Debug)]
+pub struct Day01 {
+    rotations: Vec<(bool, isize)>,
+}
 
 impl Day<'_> for Day01 {
-    type T1 = usize;
-    type T2 = usize;
+    type T1 = isize;
+    type T2 = isize;
 
     fn new(input: &str) -> Self {
-        Self {}
+        Self {
+            rotations: input
+                .lines()
+                .map(|l| {
+                    let (dir, n) = l.split_at(1);
+                    (dir == "R", n.parse::<isize>().unwrap())
+                })
+                .collect(),
+        }
     }
 
     fn part_1(&self) -> Self::T1 {
-        0
+        self.rotations
+            .iter()
+            .fold((50, 0), |(position, zeros), &(clockwise, count)| {
+                let mut new_position = (position + if clockwise { count } else { -count }) % 100;
+                new_position = if new_position < 0 {
+                    100 + new_position
+                } else {
+                    new_position
+                };
+                (new_position, zeros + if new_position == 0 { 1 } else { 0 })
+            })
+            .1
     }
 
     fn part_2(&self) -> Self::T2 {
-        0
+        self.rotations
+            .iter()
+            .fold((50isize, 0), |(position, mut zeros), &(clockwise, count)| {
+                let mut new_position = position + if clockwise { count } else { -count };
+                if clockwise {
+                    zeros += new_position / 100;
+                } else {
+                    zeros += (new_position / 100).abs();
+                }
+                zeros += if position != 0 && position.signum() != new_position.signum() {
+                    1
+                } else {
+                    0
+                };
+                new_position %= 100;
+                new_position = if new_position < 0 {
+                    100 + new_position
+                } else {
+                    new_position
+                };
+                (new_position, zeros)
+            })
+            .1
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {}
 }
